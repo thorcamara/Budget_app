2b0ba8d73c3ad534e16cb8e343a74eee70173645
thorcamara <thorcamaraalthoff@gmail.com>
2023-09-06 21:43:45 -0300

    Defined variables, constants and added EventListener

---
+++ script.js
@@ -0,0 +1,25 @@
+let totalAmount = document.getElementById("total-amount");
+let userAmount = document.getElementById("user-amount");
+const checkAmountButton = document.getElementById("check-amount");
+const totalAmountButton = document.getElementById("total-amount-button");
+const productTitle = document.getElementById("product-title");
+const errorMessage = document.getElementById("budget-error");
+const productTitleError = document.getElementById("product-title-error");
+const productCostError = document.getElementById("product-cost-error");
+const amount = document.getElementById("amount");
+const expenditureValue = document.getElementById("expenditure-value");
+const balanceValue = document.getElementById("balance-amount");
+const list = document.getElementById("list");
+let tempAmount = 0;
+
+totalAmountButton.addEventListener("click", () => {
+  tempAmount = totalAmount.value;
+  if (tempAmount === "" || tempAmount < 0) {
+    errorMessage.classList.remove("hide");
+  } else {
+    errorMessage.classList.add("hide");
+    amount.innerHTML = tempAmount;
+    balanceValue.innerText = tempAmount - expenditureValue.innerText;
+    totalAmount.value = "";
+  }
+});
