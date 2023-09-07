ac8d49c2674c7ceb85fceff422ba0697c23c9b46
thorcamara <thorcamaraalthoff@gmail.com>
2023-09-06 22:06:18 -0300

    Added more constants and variables

--- script.js
+++ script.js
@@ -23,3 +23,49 @@ totalAmountButton.addEventListener("click", () => {
     totalAmount.value = "";
   }
 });
+
+const disableButtons = (bool) => {
+  let editButtons = document.getElementsByClassName("edit");
+  Array.from(editButtons).forEach((element) => {
+    element.disabled = bool;
+  });
+};
+
+const modifyElement = (element, edit = false) => {
+  let parentDiv = element.parentElement;
+  let currentBalance = balanceValue.innerText;
+  let currentExpense = expenditureValue.innerText;
+  let parentAmount = parentDiv.querySelector(".amount").innerText;
+  if (edit) {
+    let parentText = parentDiv.querySelector(".product").innerText;
+    productTitle.value = parentText;
+    userAmount.value = parentAmount;
+    disableButtons(true);
+  }
+
+  balanceValue.innerText = parseInt(currentBalance) + parseInt(parentAmount);
+  expenditureValue.innerText = parseInt(currentExpense) - parseInt(parentAmount);
+  parentDiv.remove();
+};
+
+const listCreator = (expenseName, expenseValue) => {
+  let subListContent = document.createElement("div");
+  subListContent.classList.add("sublist-content", "flex-space");
+  list.appendChild(subListContent);
+  subListContent.innerHTML = `<p class="product">${expenseName}</p><p class="amount">${expenseValue}</p>`;
+  let editButton = document.createElement("button");
+  editButton.classList.add("fa-solid", "fa-pen-to-square", "edit");
+  editButton.style.fontSize = "1.2em";
+  editButton.addEventListener("click", () => {
+    modifyElement(editButton, true);
+  });
+  let deleteButton = document.createElement("button");
+  deleteButton.classList.add("fa-solid", "fa-trash-can", "delete");
+  deleteButton.style.fontSize = "1.2em";
+  deleteButton.addEventListener("click", () => {
+    modifyElement(deleteButton);
+  });
+  subListContent.appendChild(editButton);
+  subListContent.appendChild(deleteButton);
+  document.getElementById("list").appendChild(subListContent);
+};
